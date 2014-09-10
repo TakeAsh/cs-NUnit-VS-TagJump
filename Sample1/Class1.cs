06b7fd5222ea41a1efb4c0ca6a3bf01b95e1e1cf
TakeAsh <take@TakeAsh.net>
2014-09-10 22:14:02 +0900

    add test sample

---
+++ Sample1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sample1 {
+    static public class Class1 {
+        static public T max<T>(T a, T b)
+            where T : IComparable {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+    }
+}
---
+++ Sample1Test/Class1Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Sample1;
+
+namespace Sample1Test {
+    [TestFixture]
+    public class Class1Test {
+
+        [TestCase(1, 3, 1)]   // test fail
+        [TestCase(1, 3, 3)]
+        [TestCase(3, 1, 1)]   // test fail
+        [TestCase(3, 1, 3)]
+        public void maxTest_int(int a, int b, int expected) {
+            Assert.AreEqual(expected, Class1.max(a, b));
+        }
+
+        [TestCase(1.0, 3.0, 1.0)]   // test fail
+        [TestCase(1.0, 3.0, 3.0)]
+        [TestCase(3.0, 1.0, 1.0)]   // test fail
+        [TestCase(3.0, 1.0, 3.0)]
+        public void maxTest_double(double a, double b, double expected) {
+            Assert.AreEqual(expected,Class1.max(a,b));
+        }
+    }
+}
